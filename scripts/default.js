464bdfa3519a68cca1a9eaf111335e40da0cab79
Владислав Рузманов <alinapirajoke@gmail.com>
2023-12-02 00:53:33 +0300

    (пре) 3я лаба

---
+++ scripts/default.js
@@ -0,0 +1,37 @@
+// Получаем текущий путь страницы и сохраняем в переменную
+const currentPage = window.location.pathname; 
+
+function onLoad(){
+    updateSessionHistory(currentPage);
+    updateAllTimeHistory(currentPage);
+    showDate();
+}
+
+// Функция для обновления истории текущего сеанса в локальном хранилище
+function updateSessionHistory(page) {
+    // Получаем данные из локального хранилища или создаем пустой объект, если данных нет
+    let sessionHistory = JSON.parse(localStorage.getItem('sessionHistory')) || {};
+    // Увеличиваем счетчик посещений текущей страницы в истории текущего сеанса
+    sessionHistory[page] = (sessionHistory[page] || 0) + 1;
+    // Обновляем данные в локальном хранилище
+    localStorage.setItem('sessionHistory', JSON.stringify(sessionHistory));
+}
+
+function updateAllTimeHistory(page) {
+    // Получаем данные из sessionStorage или создаем пустой объект, если данных нет
+    let allTimeHistory = JSON.parse(sessionStorage.getItem('allTimeHistory')) || {};
+    // Увеличиваем счетчик посещений текущей страницы в общей истории
+    allTimeHistory[page] = (allTimeHistory[page] || 0) + 1;
+    // Устанавливаем обновленные данные в sessionStorage
+    sessionStorage.setItem('allTimeHistory', JSON.stringify(allTimeHistory));
+}
+
+function showDate() {
+    var months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
+    var date = new Date();
+    var label = document.getElementById('dateLabel')
+    console.log(date);
+    label.textContent = date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear();
+}
+
+window.addEventListener('load', () => { onLoad(); });
